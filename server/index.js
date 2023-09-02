2f6851319b1c0f2582d6e88eec78952821ec49e9
Maor Arie <maorarye10@gmail.com>
2023-09-03 02:46:45 +0300

    cors changes

--- server/index.js
+++ server/index.js
@@ -16,12 +16,6 @@ app.use(
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", clientUrl);
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
