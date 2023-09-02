f1f7a8f70d1632c02ae2447cb9f84a2ae5c99f04
Maor Arie <maorarye10@gmail.com>
2023-09-03 00:07:22 +0300

    Fixed server api on client

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = process.env.REACT_APP_SERVER_API_URL;
 export const registerRoute = `${host}/api/user/register`;
 export const loginRoute = `${host}/api/user/login`;
 export const setAvatarRoute = `${host}/api/user/setAvatar`;
